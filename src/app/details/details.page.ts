52a7750cebb9d6aa7959268e6b3611a01e620982
chlorella84 <dominika.sliwa@gmail.com>
2020-10-09 09:52:46 +0200

    mod8

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
+  public id: string;
+  constructor(private route: ActivatedRoute) { 
+    this.route.params.subscribe((params) =>{
+      this.id= params['id'];
+    });
+  }
 
   ngOnInit() {
   }
--- src/app/models/film.ts
+++ src/app/models/film.ts
@@ -1,9 +1,11 @@
 export class Film{
+    public imdbID: string;
     public Title : string;
     public Year : number;
     public Poster : string;
 
-    public constructor(Title : string, Year : number , Poster : string){
+    public constructor(imdbID: string, Title : string, Year : number , Poster : string){
+        this.imdbID = imdbID;
         this.Title = Title;
         this.Year = Year;
         this.Poster = Poster;
